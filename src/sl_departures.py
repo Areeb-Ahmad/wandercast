980c695881f6aa0ba61f455804d575db2af70192
Areeb-Ahmad <areeb.ahmad500@gmail.com>
2024-11-08 00:46:47 +0100

    Added code

--- src/sl_departures.py
+++ src/sl_departures.py
@@ -1,15 +1,26 @@
-#!/usr/bin/env
+#/usr/bin/env
 
-#ADD YOUR CODE HERE!!! 
+import requests
+import os
+import dotenv
+from dotenv import load_dotenv
+import tkinter
 
-class Departures:
+classs Departure:
+	def __init__(self,tk_canvas:tkinter.Canvas, city:str):
+		load_dotenv()
+		self.API_KEY = os.getenv("SL_API_KEY")
+		self.WINDOW = tk_canvas
+		self.DepartFrom = "DEPARTURE STOP"
+		self.Destination = "DESTINATION STOP" 
+		self.URL =  "someurl"
+		self.HEIGHT = self.WINDOW_winfo_height()
+		self.WIDTH = self.WINDOW_winfo_width()
 
-	def __init__(self,attributes) 
-	    self.attributes = attributes 
-	    
-	    
+	def get_departure(self):
+		try:
+			response = requests.get(self.URL)
+			data = response.json()
 
-
-
-
-
+			if response.status_code == 200:
+				self.WINDOW.delete("Departures"
